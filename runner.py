d24b96e547e46fbade658104e9c1a07c044e2b39
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2023-05-23 22:35:54 -0300

    feat: basic version

--- runner.py
+++ runner.py
@@ -19,14 +19,13 @@ if linguagem_prog not in linguagens:
     print("Linguagem de programação inválida.")
     exit(1)
 
-# Constante para rodar o arquivo
-RODAR_ARQUIVO = linguagens[linguagem_prog]["comando"] + config["nome_do_arquivo"]
-
-# Caminho para o arquivo main.hs
-MAIN_FILE = config["nome_do_arquivo"]
-
 # Pasta onde estão os arquivos de entrada (.in) e saída (.out)
 CASOS_FOLDER = config["pasta_dos_casos_de_teste"]
+
+# Criar a pasta "resultado" se não existir
+RESULTADO_FOLDER = "resultado"
+if not os.path.exists(RESULTADO_FOLDER):
+    os.makedirs(RESULTADO_FOLDER)
 
 # Listas para casos aprovados e reprovados
 aprovados = []
@@ -39,20 +38,32 @@ for in_file in os.listdir(CASOS_FOLDER):
         num_caso = os.path.splitext(in_file)[0]
         out_file = os.path.join(CASOS_FOLDER, f"{num_caso}.out")
 
-        # Ler o conteúdo do arquivo de entrada
-        with open(os.path.join(CASOS_FOLDER, in_file)) as input_file:
-            input_data = input_file.read()
+        # RODAR_ARQUIVO constant is moved inside the loop
+        RODAR_ARQUIVO = f"{linguagens[linguagem_prog]['comando']}{config['nome_do_arquivo']} < {os.path.join(CASOS_FOLDER, in_file)}"
 
         # Executar o comando para rodar o arquivo e capturar a saída
-        process = subprocess.run(RODAR_ARQUIVO, input=input_data, capture_output=True, text=True, shell=True)
+        process = subprocess.run(RODAR_ARQUIVO, capture_output=True, text=True, shell=True)
 
-        # Comparar a saída gerada com o arquivo .out correspondente
+        # Comparar a saída gerada com a arquivo .out correspondente
         if process.stdout.strip() == open(out_file).read().strip():
             # Caso aprovado
             aprovados.append(num_caso)
         else:
             # Caso reprovado
             reprovados.append(num_caso)
+
+            # Criar o arquivo .diff com a entrada, saída gerada e saída esperada
+                    # Ler o conteúdo do arquivo de entrada para escrever no .diff
+        with open(os.path.join(CASOS_FOLDER, in_file)) as input_file:
+            input_data = input_file.read()
+
+        # Criar o arquivo .diff com a entrada, saída gerada e saída esperada
+        diff_file = os.path.join(RESULTADO_FOLDER, f"{num_caso}.diff")
+        with open(diff_file, "w") as file:
+            file.write(f"Entrada:\n{input_data}\n\n")
+            file.write(f"Saída gerada:\n{process.stdout}\n\n")
+            file.write(f"Saída esperada:\n{open(out_file).read()}\n")
+
 
 # Ordenar a lista de casos aprovados
 aprovados.sort(key=int)
