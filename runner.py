b288b158755a43cc706338c257bbeadf001bde62
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2023-05-24 00:07:14 -0300

    feat: projeto básico finalizado

--- runner.py
+++ runner.py
@@ -1,66 +1,93 @@
 import os
 import subprocess
+import json
 
-# Caminho para o arquivo main.py
-MAIN_FILE = "main.py"
+LINGUAGENS = {
+    '1': {
+        'nome': 'C',
+        'comando': './',
+        'extensao': ''
+    },
+    '2': {
+        'nome': 'Python',
+        'comando': 'python3',
+        'extensao': '.py'
+    },
+    '3': {
+        'nome': 'Haskell',
+        'comando': 'runhaskell',
+        'extensao': '.hs'
+    }
+}
 
-# Pasta onde estão os arquivos de entrada (.in) e saída (.out)
-CASOS_FOLDER = "casos"
-
-# Pasta onde serão salvos os resultados
+CONFIG_FILE = 'test_config.json'
 RESULTADO_FOLDER = "resultado"
 
-# Criar a pasta "resultado" se não existir
-if not os.path.exists(RESULTADO_FOLDER):
-    os.makedirs(RESULTADO_FOLDER)
+def obter_configuracoes():
+    if not os.path.isfile(CONFIG_FILE):
+        print("Esta parece ser a primeira vez que você está rodando o script.")
+        config = {}
+        escolha_linguagem = input("Qual a linguagem que será utilizada nesse projeto?\n1- C\n2-Python\n3-Haskell\n")
+        linguagem = LINGUAGENS[escolha_linguagem]
+        config['linguagem'] = linguagem['nome']
+        config['comando'] = linguagem['comando']
+        config['extensao'] = linguagem['extensao']
+        config['nome_arquivo'] = input("Qual o nome do arquivo (sem a extensão)?\n")
+        config['caminho_testes'] = input("Qual o caminho da pasta que possui os testes?\nSe ele estiver em uma pasta aqui dentro chamada 'casos' digite apenas casos\n")
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f, indent=4)
+        print("As configurações foram salvas no arquivo 'test_config.json'.")
 
-# Listas para casos aprovados e reprovados
-aprovados = []
-reprovados = []
+def carregar_configuracoes():
+    with open(CONFIG_FILE, 'r') as f:
+        return json.load(f)
 
-# Loop para percorrer os arquivos de entrada
-for in_file in os.listdir(CASOS_FOLDER):
-    if in_file.endswith(".in"):
-        # Extrair o número do caso a partir do nome do arquivo
-        num_caso = os.path.splitext(in_file)[0]
-        out_file = os.path.join(CASOS_FOLDER, f"{num_caso}.out")
+def executar_testes():
+    config = carregar_configuracoes()
+    main_file = config['nome_arquivo'] + config['extensao']
+    command = config['comando']
+    casos_folder = config['caminho_testes']
+    
+    aprovados = []
+    reprovados = []
+    
+    for in_file in os.listdir(casos_folder):
+        if in_file.endswith(".in"):
+            num_caso = os.path.splitext(in_file)[0]
+            out_file = os.path.join(casos_folder, f"{num_caso}.out")
+    
+            with open(os.path.join(casos_folder, in_file)) as input_file:
+                input_data = input_file.read()
+    
+            process = subprocess.run([command, main_file], input=input_data, capture_output=True, text=True)
+    
+            if process.stdout.strip() == open(out_file).read().strip():
+                aprovados.append(num_caso)
+            else:
+                reprovados.append(num_caso)
+    
+            resultado_file = os.path.join(RESULTADO_FOLDER, f"{num_caso}.diff")
+            with open(resultado_file, "w") as file:
+                file.write(f"Arquivo origem da entrada: {in_file}\n")
+                file.write("Entrada:\n")
+                file.write(f"{input_data}\n\n")
+                file.write(f"Arquivo origem da saída esperada: {out_file}\n")
+                file.write("Saída esperada:\n")
+                file.write(f"{open(out_file).read()}\n\n")
+                file.write("Saída do programa:\n")
+                file.write(f"{process.stdout}\n")
+    
+    aprovados.sort(key=int)
+    
+    print("Casos aprovados:", ", ".join(aprovados))
+    print("Casos reprovados:", ", ".join(reprovados))
+    
+    total = len(aprovados) + len(reprovados)
+    print(f"Total: {len(aprovados)}/{total}")
 
-        # Ler o conteúdo do arquivo de entrada
-        with open(os.path.join(CASOS_FOLDER, in_file)) as input_file:
-            input_data = input_file.read()
+def main():
+    obter_configuracoes()
+    executar_testes()
 
-        # Executar o comando python3 e capturar a saída
-        process = subprocess.run(["python3", MAIN_FILE], input=input_data, capture_output=True, text=True)
-
-        # Comparar a saída gerada com o arquivo .out correspondente
-        if process.stdout.strip() == open(out_file).read().strip():
-            # Caso aprovado
-            aprovados.append(num_caso)
-        else:
-            # Caso reprovado
-            reprovados.append(num_caso)
-
-        # Criar o arquivo de resultado para o caso de teste
-        resultado_file = os.path.join(RESULTADO_FOLDER, f"{num_caso}.diff")
-        with open(resultado_file, "w") as file:
-            file.write(f"Arquivo origem da entrada: {in_file}\n")
-            file.write("Entrada:\n")
-            file.write(f"{input_data}\n\n")
-            file.write(f"Arquivo origem da saída esperada: {out_file}\n")
-            file.write("Saída esperada:\n")
-            file.write(f"{open(out_file).read()}\n\n")
-            file.write("Saída do programa:\n")
-            file.write(f"{process.stdout}\n")
-
-# Ordenar a lista de casos aprovados
-aprovados.sort(key=int)
-
-# Imprimir casos aprovados
-print("Casos aprovados:", ", ".join(aprovados))
-
-# Imprimir casos reprovados
-print("Casos reprovados:", ", ".join(reprovados))
-
-# Imprimir total de casos
-total = len(aprovados) + len(reprovados)
-print(f"Total: {len(aprovados)}/{total}")
+if __name__ == "__main__":
+    main()
